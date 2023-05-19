1bfadc6dc0375131daf6274c2f47259d40b757c2
YuriiPohorilets <pohorilets.y@gmail.com>
2023-05-19 21:57:55 +0300

    fixed. regex patterns

--- src/helpers/patterns.js
+++ src/helpers/patterns.js
@@ -1,4 +1,4 @@
-export const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[^_.-]\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[^_.-]\w+([.-]?\w+)*(\.\w{2,3})+$/;
 export const passwordPattern = /(?!\s)^[^ ]{7,32}$/;
-export const namePattern = /^[a-zA-zа-яіїєА-ЯІЇЄ ,.'-][^\_]+$/;
+export const namePattern = /^[a-zA-zа-яіїєА-ЯІЇЄ ,.'-][^_]+$/;
 export const phonePattern = /^380\d{9}$/;
